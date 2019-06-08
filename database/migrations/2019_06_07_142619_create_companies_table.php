8c8a65d360605beb4b6de724b689e5cd072d366f
Kyoji Katahira <katahira@agilemedia.jp>
2019-06-08 15:17:07 +0900

    change migration

--- database/migrations/2019_06_07_142619_create_companies_table.php
+++ database/migrations/2019_06_07_142619_create_companies_table.php
@@ -14,7 +14,7 @@ class CreateCompaniesTable extends Migration
     public function up()
     {
         Schema::create('companies', function (Blueprint $table) {
-            $table->tinyIncrements('id');
+            $table->bigIncrements('id');
             $table->string('name', 100);
             $table->string('address', 200);
             $table->timestamps();
